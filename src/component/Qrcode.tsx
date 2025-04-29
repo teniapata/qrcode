2b13f9ddc26082d08b6904c87262e70f13c5d4e4
teniapata <tennytoyin@gmail.com>
2025-04-29 19:20:46 +0100

    Initial commit

--- src/component/Qrcode.tsx
+++ src/component/Qrcode.tsx
@@ -2,19 +2,19 @@ const QrCode = () => {
   return (
     <div className=" bg-blue-100  justify-center flex items-center min-h-[calc(100vh-200px)] w-[calc(100vw-600px)]">
       <div className="border-4 w-[170px] shrink-0 shadow-xl bg-white h-[270px] p-1 rounded-[16px] font-bold ">
-      <img
-        src="../../src/assets/qrc.png"
-        alt=""
-        className="w-[170px] shadow-2xlz h-[150px] rounded-[5px]"
-      />
-      <p className="text-black text-[12px] pt-3 ">
-        improve your front-end skills by building projects
-      </p>
-      <p className=  "text-gray-400 pt-1  text-[8px] ">
-        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-        </p> 
+        <img
+          src="/qrc.png"
+          alt=""
+          className="w-[170px] shadow-2xlz h-[150px] rounded-[5px]"
+        />
+        <p className="text-black text-[12px] pt-3 ">
+          improve your front-end skills by building projects
+        </p>
+        <p className="text-gray-400 pt-1  text-[8px] ">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
-
     </div>
   );
 };
